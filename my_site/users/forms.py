c8076ae85f0e2b78f301580483d61915b5198984
Kirill Soloshenko <smertlove@yandex.ru>
2023-06-15 20:30:47 +0300

    try to fix forms. no success

--- my_site/users/forms.py
+++ my_site/users/forms.py
@@ -4,8 +4,20 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+
+
+    # email = forms.EmailField()
+
+    # birth_year = forms.DateField(
+    #     widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES)
+    # )
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+          'username': forms.TextInput(attrs={'class': 'input is-medium'}),
+          'email': forms.EmailInput(attrs={'class': 'input is-medium'}),
+          'password1': forms.PasswordInput(attrs={'class': 'input is-medium'}),
+          'password2': forms.PasswordInput(attrs={'class': 'input is-medium'}),
+        }
