2e681ac175c17783bfa3e05b853dab2f76a8bbf3
Fedasov <128252719+Fedasov@users.noreply.github.com>
2023-07-01 02:48:08 +0300

    Add files via upload

--- my_site/inopolis/urls.py
+++ my_site/inopolis/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from users import views as user_views
+from inopolis import views
 
 urlpatterns = [
-	path('', views.index, name='index'),   #blog-home
-	path('about/', views.film_page, name='film_page'),   #blog-about
+	path('', views.index, name='index'),
+	path('about/', views.film_page, name='film_page'),
 ]
