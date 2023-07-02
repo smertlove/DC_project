d08b9d0e581610df7a69f2d820c89729c361b54d
Kirill Soloshenko <smertlove@yandex.ru>
2023-07-02 20:28:42 +0300

    get rid of misworking code

--- my_site/users/forms.py
+++ my_site/users/forms.py
@@ -52,13 +52,8 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
-        widgets = {
-          'username': forms.TextInput(attrs={'class': 'input is-medium'}),
-          'email': forms.EmailInput(attrs={'class': 'input is-medium'}),
-          'password1': forms.PasswordInput(attrs={'class': 'input is-medium'}),
-          'password2': forms.PasswordInput(attrs={'class': 'input is-medium'}),
-        }
+        fields = ['username', 'email', 'password', 'confirm_password']
+
 
 
 class ProfileForm(ModelForm):
