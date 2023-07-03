054fb73f40f678e0918452172c6bb9e56a134e4a
Kirill Soloshenko <smertlove@yandex.ru>
2023-07-04 01:45:33 +0300

    add color theme switch

--- my_site/inopolis/static/inopolis/js/ColorThemeSwitch.js
+++ my_site/inopolis/static/inopolis/js/ColorThemeSwitch.js
@@ -2,19 +2,66 @@
 
 // from -- CSS class, to -- CSS class
 function toggleColorTheme(from, to) {
-    console.log(1);
+
+    let elemsToChange = document.getElementsByClassName(from);
+
+    Array.from(elemsToChange).forEach(element => {
+        element.classList.remove(from);
+        element.classList.add(to);
+    });
+
     return
 }
+
+
 
 // elem1 -- DOM elem to hide, elem2 -- DOM elem to reveal
 function switchVisibility (elem1, elem2) {
-    console.log(2);
+    elem2.classList.add("is-hidden");
+    elem1.classList.remove("is-hidden");
     return
 }
 
+
 var colorSwitch = document.getElementById("ColorThemeSwitch");
-console.log(colorSwitch);
+var mainFrom = "is-dark";
+var mainTo = "is-light";
+var bodyFrom = "body-black";
+var bodyTo = "body-white";
+var footerFrom = "my_footer-black";
+var footerTo = "my_footer-white";
+var textFrom = "all-text-white";
+var textTo = "all-text-black";
+
+var currentSVG = document.getElementById("toggle-moon-id");
+var hiddenSVG = document.getElementById("toggle-sun-id");
+
+
 colorSwitch.onclick = function() {
-    switchVisibility(1, 2);
-    toggleColorTheme(1, 2);
+    switchVisibility(currentSVG, hiddenSVG);
+    let temp = currentSVG;
+    currentSVG = hiddenSVG;
+    hiddenSVG = temp;
+
+    toggleColorTheme(mainFrom, mainTo);
+    temp = mainFrom;
+    mainFrom = mainTo;
+    mainTo = temp;
+
+    toggleColorTheme(bodyFrom, bodyTo)
+    temp = bodyFrom;
+    bodyFrom = bodyTo;
+    bodyTo = temp;
+
+    toggleColorTheme(footerFrom, footerTo)
+    temp = footerFrom;
+    footerFrom = footerTo;
+    footerTo = temp;
+
+    toggleColorTheme(textFrom, textTo)
+    temp = textFrom;
+    textFrom = textTo;
+    textTo = temp;
+
+
 }
