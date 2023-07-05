23f782901e24325c897899010800f95cfb17cf76
Fedasov <fedasov03@inbox.ru>
2023-07-05 18:10:07 +0300

    внес изменения в профильл пользователя и для страницы редактирования информации о себе

--- my_site/users/models.py
+++ my_site/users/models.py
@@ -2,14 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio = models.TextField(null=True, blank=True)
     profile_pic = models.ImageField(null=True, blank=True, upload_to="avatarka/")
     instagram = models.CharField(max_length=50, null=True, blank=True)
-    date_of_birth = models.DateField(default=datetime.date.today())  #input_formats=['%d/%m/%Y'],
-    # email = models.CharField(max_length=50, on_delete=)
+    date_of_birth = models.DateField(null=True, blank=True)  #input_formats=['%d/%m/%Y'],
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
     def __str__(self):
         return str(self.user)
 
--- my_site/users/urls.py
+++ my_site/users/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from users import views as user_views
-from users import views as user_views
 from django.contrib.auth import views as auth_views
 from .views import *
 
@@ -11,6 +10,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html', authentication_form=UserLoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('user_profile/<int:pk>/', ShowProfilePageView.as_view(), name='user_profile'),
-    path('create_profile_page/', CreateProfilePageView.as_view(), name='create_user_profile'),   #CreateProfilePageView.as_view()
-    path('edit_profile_page/<int:pk>/', EditProfilePageView.as_view(), name='edit_user_profile'),
+   # path('create_profile_page/', CreateProfilePageView.as_view(), name='create_user_profile'),   #CreateProfilePageView.as_view()
+    path('edit_profile_page/', user_views.edit_profile, name='edit_user_profile'),
+    path('edit_pass/', user_views.edit_pass, name='edit_pass'),
+
 ]
