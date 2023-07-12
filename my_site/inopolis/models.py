3fc649b84a3831bbcd64fe4db7596e63d60aae15
Kirill Soloshenko <smertlove@yandex.ru>
2023-07-12 20:52:46 +0300

    черновик моделей

--- my_site/inopolis/models.py
+++ my_site/inopolis/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+
+    name = models.CharField()
+    letter= models.CharField()
+
+
+
+class Film(models.Model):
+
+    title = models.CharField(max_length=64)
+    genre = models.ManyToManyField("Genre")
+    description = models.TextField()
+    rating = models.CharField()
+    director = models.CharField(max_length=64)
+    producer = models.CharField(max_length=64)
+    writer = models.CharField(max_length=64)
+    release_date_theaters = models.DateField()
+    release_date_streaming = models.DateField()
+
+
+class Comment(models.Model):
+
+    user = models.ForeignKey("User", on_delete=models.PROTECT)
+    film = models.ForeignKey("Film", on_delete=models.PROTECT)
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
